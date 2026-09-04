39439dd362c091003bce1a515256204ea2c95be9
jessyaz <jessyazizi08@gmail.com>
2026-09-04 09:55:29 +0200

    Transformers added : epf-transformer + timexer

--- models/git-src/clone_models.sh
+++
@@ -1,1 +0,0 @@
-git clone https://github.com/osllogon/epf-transformers.git
---
+++ models/git_src/clone_models.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/osllogon/epf-transformers.git
+
+
+cd models/git-src/epf-transformers
+rm -rf .git .github README.md
+rm -f src/benchmark.py src/train.py src/train_functions.py src/utils.py src/visualizations.py
+rm pre_commit.sh  requirements.txt src/exploratory.ipynb
